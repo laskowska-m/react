307035ca007ddd1c6783148fe5b433f3813dad65
laskowska-m <laskowska.mt@gmail.com>
2022-12-05 19:42:43 +0100

    add row-reverse to section

--- landing-page/src/components/ImageTextSection/ImageTextSection.js
+++ landing-page/src/components/ImageTextSection/ImageTextSection.js
@@ -3,10 +3,10 @@ import HeadingWithParagraph from '../HeadingWithParagraph/HeadingWithParagraph';
 
 export default class ImageTextSection extends Component {
     render() {
-        const { image, title, p1, p2, headingLevel } = this.props;
+        const { image, title, p1, p2, headingLevel, className } = this.props;
 
         return (
-            <div className="gap-[30px] mx-auto flex justify-between items-center">
+            <div className={className ? `gap-[30px] mx-auto flex justify-between items-center ${className}` : "gap-[30px] mx-auto flex justify-between items-center"}>
                 <div className='w-1/2'>
                     <HeadingWithParagraph title={title} headingLevel={headingLevel} p1={p1} p2={p2} />
 
--- landing-page/src/pages/Home.js
+++ landing-page/src/pages/Home.js
@@ -12,14 +12,14 @@ import sectionIcon from '../img/section_icon.svg';
 
 export default function Home() {
     return (
-        <div className='max-w-[1100px] mx-auto '>
+        <div className='max-w-[1100px] mx-auto'>
             <HeroSection />
             <section className='pt-40'>
                 <ImageTextSection image={sectionImg1} headingLevel='h2' title='Light, Fast & Powerful' p1='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus' p2='mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.' />
                 <IconTitleTextList headingLevel='h3' icon={sectionIcon} title='Title Goes Here' p='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.' />
             </section>
             <section className='py-40'>
-                <ImageTextSection image={sectionImg2} headingLevel='h2' title='Light, Fast & Powerful' p1='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus' p2='mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.' />
+                <ImageTextSection className='flex-row-reverse' image={sectionImg2} headingLevel='h2' title='Light, Fast & Powerful' p1='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus' p2='mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.' />
             </section>
 
         </div>
